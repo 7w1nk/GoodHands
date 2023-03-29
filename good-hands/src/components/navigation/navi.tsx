6d459667a44ef79a47a8536e8a9aac380d1a4d73
Alexandr Papushev <alepap2311@yandex.ru>
2023-03-29 20:10:59 +0300

    Add background

--- good-hands/src/components/navigation/navi.tsx
+++ good-hands/src/components/navigation/navi.tsx
@@ -1,25 +1,27 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export interface LinkProp {
-    id: string,
-    name: string
+	id: string;
+	name: string;
 }
 
 export const ListLinkProp: LinkProp[] = [
-    {id: "catalog", name: "Каталог животных"},
-    {id: "ads", name: "Объявления"},
-    {id: "services", name: "Услуги"},
-    {id: "consultation", name: "Консультация"}
-]
+	{ id: "catalog", name: "Каталог животных" },
+	{ id: "ads", name: "Объявления" },
+	{ id: "services", name: "Услуги" },
+	{ id: "consultation", name: "Консультация" },
+];
 const Navi = () => {
-    return (
-        <>
-            {ListLinkProp.map((list: LinkProp) => (
-                <NavLink to={list.id}>{list.name}</NavLink>
-            ))}
-        </>
-    )
-}
+	return (
+		<>
+			{ListLinkProp.map((list: LinkProp) => (
+				<NavLink className={"home-navigation_link"} to={list.id}>
+					{list.name}
+				</NavLink>
+			))}
+		</>
+	);
+};
 
-export default Navi
+export default Navi;
--- good-hands/src/components/pages/homePage/homePage.tsx
+++ good-hands/src/components/pages/homePage/homePage.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useRef, CSSProperties } from "react";
 import HomePage_nav from "./homePage_nav/homePage_nav";
 import HomePage_menu from "./homePage_menu/homePage_menu";
 
 const HomePage = () => {
-    return (
-        <>
-            <HomePage_nav/>
-            <HomePage_menu/>
-        </>
-    )
+	return (
+		<>
+			<HomePage_nav />
+			<HomePage_menu />
+		</>
+	);
 };
 
 export default HomePage;
