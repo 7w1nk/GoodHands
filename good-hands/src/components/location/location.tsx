2425f6938ee4aa028f12e44e6c74ac5d348eec19
Alexandr Papushev <alepap2311@yandex.ru>
2023-03-31 11:20:48 +0300

    Add cities

--- good-hands/src/components/location/location.tsx
+++ good-hands/src/components/location/location.tsx
@@ -1,11 +1,31 @@
-import locator from "../../img/Location.svg"
+import { useEffect, useState } from "react";
+import locator from "../../img/Location.svg";
+import ListCities from "./ListCity.json";
+
+const [currentCity, setCurrentCity] = useState("");
+
+useEffect(() => {
+	const searchCity = [];
+	ListCities.forEach((city, index) => {
+		Object.values(city).every((val, valIndex) => {
+			if (val.toLowerCase().includes(currentCity.toLowerCase())) {
+				searchCity.push(city);
+				return;
+			}
+		});
+	});
+}, [currentCity]);
 
 const Location = () => {
-    return (
-        <span className="home-navigation_location">
-            <img alt="Point of location" src={locator}/>
-            <span>Красноярск</span>
-        </span>
-    )
-}
-export default Location
+	return (
+		<span className="home-navigation_location">
+			<img alt="Point of location" src={locator} />
+			<span>
+				{ListCities.map((data) => {
+					return data.city.match("Красноярск");
+				})}
+			</span>
+		</span>
+	);
+};
+export default Location;
